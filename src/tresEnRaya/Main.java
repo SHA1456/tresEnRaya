d1278056f3fee68fba94894df6a9c29068875d4c
Sergio <heredia.acero.sergio@iescamas.es>
2025-11-13 19:57:43 +0100

    tresenrayaupdate

--- src/tresEnRaya/Main.java
+++ src/tresEnRaya/Main.java
@@ -2,8 +2,78 @@ package tresEnRaya;
 import java.util.Scanner;
 public class Main {
 	
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int opcion;
+		
+		do {
+			
+			MostrarMenu();
+			
+			opcion=sc.nextInt();
+			
+			if(opcion==1) {
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			}else if(opcion==2){
+				Mostrarinstrucciones();
+			}else if(opcion<1||opcion>3) {
+				System.err.println("Opción inválida");
+			}
+			
 	
+			
+		}while (opcion!=3);
+		
+		
+		
+		
+		
+	}
+	public static void MostrarMenu () {
+		System.out.println("Opciones: 1) Jugar, 2)Mostrar instrucciones, 3)Salir");
+		
+	}
 	
+	public static void Mostrarinstrucciones() {
+		System.out.println("Instrucciones del tres en raya");
+		
+	}
 	
+	public static void Inicializartablero () {
+		
+		
+	}
+	
+	public static void mostrarTablero() {
+		
+	}
+	
+	public static void hayGanador() {
+		
+	}
+	
+	public static void tableroLleno () {
+		
+	}
 	
 }
